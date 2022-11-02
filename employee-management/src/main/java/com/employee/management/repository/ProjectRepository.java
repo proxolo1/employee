911e060fc364f4362ddda26079087bd809bc001e
Ajay K Santhosh <73517233+proxolo1@users.noreply.github.com>
2022-11-02 15:17:41 +0530

    Delete employee-management directory

--- employee-management/src/main/java/com/employee/management/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.employee.management.repository;
-
-import com.employee.management.model.ProjectModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<ProjectModel,String> {
-}
